985e9ba25e40a6ae0e7f493f7e0d47b3f52c9e42
Dhruv Sahu <73589214+Dhruvsahu98@users.noreply.github.com>
2021-10-03 18:42:36 +0530

    Create start-stop-Update.sol

---
+++ remixbackup/.workspaces/default_workspace/start-stop-Update.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.13;
+
+contract StartStopUpdateExample {
+    address owner;
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function sendMoney() public payable {
+        
+    }
+    
+    function withdrawAllMoney(address payable _to) public {
+        require(msg.sender == owner,"You are not the owner");
+        _to.transfer(address(this).balance);
+    }
+}
