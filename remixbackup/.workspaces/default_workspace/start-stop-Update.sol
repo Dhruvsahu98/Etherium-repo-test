cb76b3b2ca5f9ced3ed3d886053f609480a3d19c
Dhruv Sahu <73589214+Dhruvsahu98@users.noreply.github.com>
2021-10-04 13:10:34 +0530

    added destroy method
    added a destroy function

--- remixbackup/.workspaces/default_workspace/start-stop-Update.sol
+++ remixbackup/.workspaces/default_workspace/start-stop-Update.sol
@@ -6,7 +6,7 @@ contract StartStopUpdateExample {
     bool public paused;
     
     function setPaused(bool _paused) public {
-                require(msg.sender == owner,"You are not the owner");   //When contract is paused we can send money but cannot withdraw money from root account.
+                require(msg.sender == owner,"You are not the owner");  //when contract is paused I can send money but cannot withdraw money
                  paused = _paused;
     }
     
@@ -22,5 +22,10 @@ contract StartStopUpdateExample {
         require(msg.sender == owner,"You are not the owner");
         require(!paused , "Your contract is paused");
         _to.transfer(address(this).balance);
+    }
+    
+    function destroySmartContract(address payable _to) public {
+         require(msg.sender == owner,"You are not the owner");
+        selfdestruct(_to);  //selfdestruct is used to destroy a smart contract
     }
 }
